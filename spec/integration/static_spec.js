892e7298c032c5803aac036e5ae5737bac2f2252
Steve <lorenz21@hawaii.edu>
2018-10-04 12:41:19 -1000

    Checkpoint 1 - Introduction to express: completed

---
+++ spec/integration/static_spec.js
@@ -0,0 +1,26 @@
+const request = require('request');
+const server = require('../../src/server');
+const base = "http://localhost:3000/";
+
+describe("routes: static", () => {
+	 
+	describe("GET /", () => {
+		it("should return status code 200", (done) => {
+			request.get(base, (err, res, body) => {
+				expect(res.statusCode).toBe(200);
+				done();
+			});
+		});
+	});
+	 
+	describe("GET /marco", () => {
+		it("should return status code 200 from /marco", (done) => {
+			request.get(base + 'marco', (err, res, body) => {
+				expect(res.statusCode).toBe(200);
+				expect(res.body).toBe("polo");
+				done();
+			});
+		});
+	});
+
+});
