a1d2b5218b08804779a30afb6d6c0364066cf898
Steve <lorenz21@hawaii.edu>
2018-10-10 19:13:27 -1000

    update test not passing with validation

--- src/db/queries.posts.js
+++ src/db/queries.posts.js
@@ -37,7 +37,6 @@ module.exports = {
          if(!post){
             return callback("Post not found");
          }
-         
          post.update(updatedPost, {
             fields: Object.keys(updatedPost)
          })
--- src/routes/posts.js
+++ src/routes/posts.js
@@ -2,6 +2,7 @@ const express = require("express");
 const router = express.Router();
 
 const postController = require("../controllers/postController")
+const validation = require('./validation');
 
 router.get("/topics/:topicId/posts/new", postController.new);
 
@@ -9,9 +10,9 @@ router.get("/topics/:topicId/posts/:id", postController.show);
 
 router.get("/topics/:topicId/posts/:id/edit", postController.edit);
 
-router.post("/topics/:topicId/posts/:id/update", postController.update);
+router.post("/topics/:topicId/posts/create", validation.validatePosts, postController.create);
 
-router.post("/topics/:topicId/posts/create", postController.create);
+router.post("/topics/:topicId/posts/:id/update", validation.validatePosts, postController.update);
 
 router.post("/topics/:topidId/posts/:id/destroy", postController.destroy);
 
