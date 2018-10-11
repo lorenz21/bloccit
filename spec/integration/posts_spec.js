263ace96332e05d4e6a49a895e598f07d3e80c87
Steve <lorenz21@hawaii.edu>
2018-10-11 09:31:57 -1000

    Checkpoint 8 - Validations: completed

--- spec/integration/posts_spec.js
+++ spec/integration/posts_spec.js
@@ -155,16 +155,18 @@ describe("routes : posts", () => {
           body: "I love watching them melt slowly."
         }
        }, (err, res, body) => {
-        expect(res.statusCode).toBe(302);
-        done();
-       });
+          expect(res.statusCode).toBe(302);
+          done();
+        }
+       );
     });
 
     it("should update the post with the given values", (done) => {
       const options = {
         url: `${base}/${this.topic.id}/posts/${this.post.id}/update`,
         form: {
-          title: "Snowman Building Competition"
+          title: "Snowman Building Competition",
+          body: "I love watching them melt so slowly."
         }
       };
       request.post(options, 
--- src/routes/topics.js
+++ src/routes/topics.js
@@ -2,19 +2,20 @@ const express = require("express");
 const router = express.Router();
 
 const topicController = require("../controllers/topicController");
-
-router.get("/topics", topicController.index);
+const validation = require('./validation');
 
 router.get("/topics/new", topicController.new);
 
 router.get("/topics/:id", topicController.show);
 
+router.get("/topics", topicController.index);
+
 router.get("/topics/:id/edit", topicController.edit);
 
-router.post("/topics/create", topicController.create);
+router.post("/topics/create", validation.validateTopics, topicController.create);
+
+router.post("/topics/:id/update", validation.validateTopics, topicController.update);
 
 router.post("/topics/:id/destroy", topicController.destroy);
-
-router.post("/topics/:id/update", topicController.update);
 
 module.exports = router;
